7c7ba5eec4361319dc2c736757ec4aeca4c7c814
nombumurage <nombumurage@gmail.com>
2017-05-17 17:34:00 +0300

    Weather.py commented code

--- weather.py
+++ weather.py
@@ -10,29 +10,30 @@ diff = []
 
 for i in range(2,len(lines)):
     removedSpaces = lines[i].strip()
-    eachcol = removedSpaces.split()
+    eachcolumn = removedSpaces.split()
 
     try:
         # For values without the asteric
-        if ("*" not in eachcol[1] and "*" not in eachcol[2]):
-            difference = int(eachcol[1]) - int(eachcol[2])
-            diff.append(eachcol[0]+ " " +str(difference))
+        if ("*" not in eachcolumn[1] and "*" not in eachcolumn[2]):
+            difference = int(eachcolumn[1]) - int(eachcolumn[2])
+            diff.append(eachcolumn[0]+ " " +str(difference))
 
         # Values with asteric for column MxT.  This is supposed to remove the asteric
-        if (eachcol[1].endswith("*")):
-            eachcol[1] = eachcol[1][:-1]
-            difference = int(eachcol[1]) - int(eachcol[2])
-            diff.append(eachcol[0]+ " " +str(difference))
+        elif (eachcolumn[1].endswith("*")):
+            eachcolumn[1] = eachcolumn[1][:-1]
+            difference = int(eachcolumn[1]) - int(eachcolumn[2])
+            diff.append(eachcolumn[0]+ " " +str(difference))
 
-        # Values with asteric for column MxT.  This is supposed to remove the asteric
-        elif (eachcol[2].endswith("*")):
-            eachcol[2]= eachcol[2][:-1]
-            difference = int(eachcol[1]) - int(eachcol[2])
-            diff.append(eachcol[0]+ " " +str(difference))
+        # Values with asteric for column MnT.  This is supposed to remove the asteric
+        elif (eachcolumn[2].endswith("*")):
+            eachcolumn[2]= eachcolumn[2][:-1]
+            difference = int(eachcolumn[1]) - int(eachcolumn[2])
+            diff.append(eachcolumn[0]+ " " +str(difference))
 
     # This deals with the float values at the very end of the dataset
     except ValueError:
-        difference = float(eachcol[1]) - float(eachcol[2])
+        difference = float(eachcolumn[1]) - float(eachcolumn[2])
         diff.append(difference)
-
+        
+# Prints out the highest temperature spread
 print max(diff)
