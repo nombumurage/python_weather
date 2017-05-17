821fc988371c73dbafe6e94308d5133ecdc961ba
nombumurage <nombumurage@gmail.com>
2017-05-17 17:29:38 +0300

    Functioning script and readme.txt

--- readme.txt
+++ readme.txt
@@ -1,7 +1,18 @@
 #Solution
-
 Pseudo code
 1. Step one would be to read the data from the external file ('weather.dat')
-2. Step two would be to write the raw data into a panda Dataframe. It is easier to manipulate table-like data with pandas.
-3. Get the difference of the two columns using
-4. Sort the values of the
+2. Load the data from the .dat file as lists of lists.
+3. Initialize an empty array which will be used to output the final result
+3. Clean the data to remove spaces.
+4. Identify each column as arrays.
+5. Loop through each data element in each column and subtract it from the other column.
+6. Add the resulting difference to the array.
+7. While appending the result add the corresponding day value from the first column (Dy).
+8. Get the highest temperature spread using max() method and print it.
+
+#Assumptions
+The script runs true for the specific dataset. Any changes in the dataset might affect the functioning of the script
+
+#To Run the Program
+1. Ensure python is installed on the machine. Version used for this script is 2.7.12
+2. Run : python weather.py
--- weather.py
+++ weather.py
@@ -1,23 +1,11 @@
-# import numpy as np
-# import pandas as pd
-
-# read weather.dat to a list of lists
-# datContent = [i.strip().split() for i in open("weather.dat").readlines()]
-# df = pd.DataFrame(datContent)
-#
-# column1 = df.ix [:, 1]
-# for i in column1:
-#
-# print(intColumn1)
-
-# Calulate the difference between the highest temperature points(*)
-# print(df.ix[[27],1])
-# print(df.ix[[10],2])
-
+# Variable that holds the external data file
 filename = "weather.dat"
 
+# Code to open and read the .dat file
 with open(filename) as text_file:
      lines = text_file.read().splitlines()
+
+# Initialize empty array to store the result
 diff = []
 
 for i in range(2,len(lines)):
@@ -25,18 +13,26 @@ for i in range(2,len(lines)):
     eachcol = removedSpaces.split()
 
     try:
+        # For values without the asteric
         if ("*" not in eachcol[1] and "*" not in eachcol[2]):
             difference = int(eachcol[1]) - int(eachcol[2])
-            diff.append(difference)
-        else:
-            eachcol[1]= eachcol[1][:-1]
-            eachcol[2] = eachcol[2][:-1]
-            difference = int(eachcol[1]) - int(eachcol[2])
-            diff.append(difference)
+            diff.append(eachcol[0]+ " " +str(difference))
 
+        # Values with asteric for column MxT.  This is supposed to remove the asteric
+        if (eachcol[1].endswith("*")):
+            eachcol[1] = eachcol[1][:-1]
+            difference = int(eachcol[1]) - int(eachcol[2])
+            diff.append(eachcol[0]+ " " +str(difference))
+
+        # Values with asteric for column MxT.  This is supposed to remove the asteric
+        elif (eachcol[2].endswith("*")):
+            eachcol[2]= eachcol[2][:-1]
+            difference = int(eachcol[1]) - int(eachcol[2])
+            diff.append(eachcol[0]+ " " +str(difference))
+
+    # This deals with the float values at the very end of the dataset
     except ValueError:
         difference = float(eachcol[1]) - float(eachcol[2])
         diff.append(difference)
 
-print diff
 print max(diff)
